19771b286aa08c52c3b959ab59f9c7f0c29d738a
MoseNjau <moses.njau@students.jkuat.ac.ke>
2024-06-10 18:15:48 +0300

    Revert to intial noreply

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,6 +1,7 @@
 <?php
 // Define email configuration variables
 $recipient_email = 'ndovu7730@gmail.com'; // Your email address where form submissions are sent
+$from_email = 'noreply@yourdomain.com'; // A no-reply email address from your domain
 
 // Check for empty fields
 if(empty($_POST['name']) || empty($_POST['email']) || empty($_POST['phone']) || empty($_POST['message']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
@@ -18,16 +19,14 @@ $message = filter_var($_POST['message'], FILTER_SANITIZE_STRING);
 $email_subject = "Website Contact Form: $name";
 $email_body = "You have received a new message from your website contact form.\n\nHere are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
 
-$headers = "From: $email_address\n"; // Email will appear to come from the user's email address
-$headers .= "Reply-To: $email_address"; // Replies will go to the user's email address
+$headers = "From: $from_email\n";
+$headers .= "Reply-To: $email_address";
 
-// Send email and check for errors
 if(mail($recipient_email, $email_subject, $email_body, $headers)) {
     echo "Message sent successfully!";
     return true;
 } else {
     echo "Message sending failed!";
-    error_log("Mail function failed: " . print_r(error_get_last(), true));
     return false;
 }
 ?>
